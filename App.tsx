129c7a39a9949dc62f1d58759d4044b877fd3573
Jon Sherrard <jon@devular.com>
2022-11-30 22:18:13 +0000

    basic

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, TamaguiProviderProps } from 'tamagui';
+import config from './tamagui.config';
+
+function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
+  return (
+    <TamaguiProvider
+      config={config}
+      disableInjectCSS
+      defaultTheme="light"
+      {...rest}
+    >
+      {children}
+    </TamaguiProvider>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
